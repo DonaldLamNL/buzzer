189fdf70f2249c1d9b2a01f5831ec6b104bc558b
Donald Lam <manholam8@gmail.com>
2023-03-17 15:27:05 +0800

    Add README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Buzzer Project
+- This is the project of CSCI3100 Group A1 in the 2022-2023 2nd semester
+
+## Start the project
+- make sure you have already installed `yarn` and `vite`
+    ```bash
+    npm -g install yarn
+    yarn --version
+
+    npm -g install vite
+    vite --version
+    ```
+
+- run the project in local
+    ```bash
+    yarn dev
+    ```
--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Hello World fire</h1>
+      </div> */}
+      <h1>Hello World</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
