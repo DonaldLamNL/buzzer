6d0cb3974c6192b629725e54cbc2ad0a7f200a04
Donald Lam <manholam8@gmail.com>
2023-03-17 17:04:03 +0800

    Install Material-UI

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Import React-Router
+import { BrowserRouter } from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
