03902a8a0f8107aba87102a7160abc7b06185a29
Donald Lam <manholam8@gmail.com>
2023-03-18 17:14:15 +0800

    Finish MainRouter

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,11 +6,23 @@ import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function index() {
   return (
-    <div style={{height: '100vh'}}>
-      <List>
+    <div style={{height: '100vh', backgroundColor: '#f4aaff'}}>
+      <ul>
+        <li>
+            <Link to="/">Home</Link>
+        </li>
+        <li>
+            <Link to="/user">User</Link>
+        </li>
+        <li>
+            <Link to="/hive">Hive</Link>
+        </li>
+      </ul>
+      {/* <List>
         {["All mail", "Trash", "Spam"].map((text, index) => (
           <ListItem key={text} disablePadding sx={{ display: "block" }}>
             <ListItemButton
@@ -33,7 +45,7 @@ export default function index() {
             </ListItemButton>
           </ListItem>
         ))}
-      </List>
+      </List> */}
     </div>
   )
 }
