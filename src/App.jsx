1128a650f90f294a24ed1f5f5c9e874267f1298f
Circle <a859576084@gmail.com>
2023-03-20 00:19:31 +0800

    work on Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { Grid } from "@mui/material";
 import { useRoutes } from "react-router-dom";
 import Nav from "./components/Nav";
-import './index.css'
+import "./index.css";
 import routerConfig from "./router";
-    
+
 export default function App() {
   const element = useRoutes(routerConfig);
   return (
     <>
       <Grid container>
-        <Grid item xs={2}>
-          <Nav/>
+        <Grid xs={2}>
+          <Nav />
         </Grid>
-        <Grid item xs={10}>
-          {element}
-        </Grid>
+        <Grid xs>{element}</Grid>
       </Grid>
     </>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,58 +1,67 @@
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-import React from 'react'
-import { Link } from "react-router-dom";
-import { positions } from "@mui/system";
+import { Box, Card, Container, IconButton, Stack } from "@mui/material";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import FaceIcon from "@mui/icons-material/Face";
+import HiveIcon from "@mui/icons-material/Hive";
+import { useNavigate } from "react-router-dom";
 
-export default function index() {
+export default function Nav() {
+  const navigate = useNavigate();
   return (
-    <div 
-      style={{
-        height: '100%',
-        backgroundColor: '#f4aaff',
-        // position: 'fixed'
-      }}
+    <Box
+      minHeight="100vh"
+      height="100%"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
     >
-      <ul>
-        <li>
-            <Link to="/home">Home</Link>
-        </li>
-        <li>
-            <Link to="/user">User</Link>
-        </li>
-        <li>
-            <Link to="/hive">Hive</Link>
-        </li>
-      </ul>
-      {/* <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding sx={{ display: "block" }}>
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
+      <Card
+        elevation={14}
+        sx={{
+          marginY: 2,
+          backgroundColor: "#3666fa",
+          borderRadius: 4,
+          height: "97%",
+          width: "40%",
+        }}
+      >
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          marginY={10}
+          spacing={10}
+        >
+          <Box>
+            <IconButton
+              size="large"
+              onClick={() => {
+                navigate("../home");
               }}
             >
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
+              <HomeRoundedIcon sx={{ fontSize: 48 }} />
+            </IconButton>
+          </Box>
+          <Box>
+            <IconButton size="large">
+              <FaceIcon
+                sx={{ fontSize: 48 }}
+                onClick={() => {
+                  navigate("../user");
                 }}
-              >
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List> */}
-    </div>
-  )
+              />
+            </IconButton>
+          </Box>
+          <Box>
+            <IconButton size="large">
+              <HiveIcon
+                sx={{ fontSize: 48 }}
+                onClick={() => {
+                  navigate("../hive");
+                }}
+              />
+            </IconButton>
+          </Box>
+        </Stack>
+      </Card>
+    </Box>
+  );
 }
