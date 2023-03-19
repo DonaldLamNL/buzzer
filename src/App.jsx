6ef7af5a33da662a0cb5a7bdc7290e0dd4a121f5
Donald Lam <manholam8@gmail.com>
2023-03-20 00:30:32 +0800

    work on sidecontent

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
-import './index.css'
-import routerConfig from "./router"
+import { Grid } from "@mui/material";
+import { useRoutes } from "react-router-dom";
+import Nav from "./components/Nav";
+import "./index.css";
+import routerConfig from "./router";
 
-// material-ui
-import { Grid } from "@mui/material"
-import { useRoutes } from "react-router-dom"
-
-// components
-import Nav from "./components/Nav"
-    
 export default function App() {
-  const element = useRoutes(routerConfig)
+  const element = useRoutes(routerConfig);
   return (
     <>
       <Grid container>
-        <Grid item xs={2}>
-          <Nav/>
+        <Grid xs={2}>
+          <Nav />
         </Grid>
-        <Grid item xs={10}>
-          {element}
-        </Grid>
+        <Grid xs>{element}</Grid>
       </Grid>
     </>
-  )
+  );
 }
--- src/router.jsx
+++ src/router.jsx
@@ -5,28 +5,28 @@ import MainContent from "./components/Content/MainContent";
 import BuzzSearch from "./components/Content/BuzzSearch";
 
 const routerConfig = [
-    {
-        path: "/*",
-        element: <Home />,
-        children:[
-            {
-                path: 'home',
-                element: <MainContent />
-            },
-            {
-                path: 'search',
-                element: <BuzzSearch />
-            }
-        ]
-    },
-    {
-      path: "/user",
-      element: <User />,
-    },
-    {
-      path: "/hive",
-      element: <Hive />,
-    },
-]
+  {
+    path: "/*",
+    element: <Home />,
+    children: [
+      {
+        path: "home",
+        element: <MainContent />,
+      },
+      {
+        path: "search",
+        element: <BuzzSearch />,
+      },
+    ],
+  },
+  {
+    path: "/user",
+    element: <User />,
+  },
+  {
+    path: "/hive",
+    element: <Hive />,
+  },
+];
 
-export default routerConfig
+export default routerConfig;
