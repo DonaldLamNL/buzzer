0b417b2b590e39b67f7adf5fe297ef818e767403
ffedmund <myemail>
2023-03-22 03:28:26 +0800

    improve Hive

--- src/components/Hive.jsx
+++ src/components/Hive.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState }from "react";
 import {
   HexGrid,
   Layout,
@@ -16,43 +16,87 @@ export default function Hive() {
   const hiveStyle = {
     height: "100vh",
     width: "100%",
-    backgroundColor: "#f5ffaa",
+    // backgroundColor: "#f5ffaa",
     // overflow:"auto",
+  };
+
+  // const [datalist, setList] = useState("");
+  // const listItem = datalist.map((d) => <HexGridItem>{d.context}</HexGridItem>);
+
+  const [value, setValue] = useState("");
+  const handleChange = e => {
+    console.log(`typed => ${e.target.value}`);
+    setValue(e.target.value);
+    const temp = datalist.push("context",e.target.value);
+    // setList(temp);
+    // const listItem = datalist.map((d) => <HexGridItem>{d.context}</HexGridItem>);
+
   };
 
   const datalist = [
     { context: "Hello" },
     { context: "World" },
     { context: "!!" },
+    { context: "Fuck" },
+    { context: "Fuck" },
+    { context: "Fuck" },
+    { context: "Fuck" },
+    { context: "Fuck" },
+    { context: "Fuck" },
+    { context: "Fuck" },
+    { context: "Fuck" },
+    { context: "Fuck" },
+    { context: "Fuck" },
+    { context: value}
   ];
-  const listItem = datalist.map((d) => <HexGridItem>{d.context}</HexGridItem>);
 
-  var hive = new Hex();
-
-  const hexagons = GridGenerator.hexagon(3);
+  // const hexagons = GridGenerator.hexagon(3);
 
   return (
     <div style={hiveStyle}>
-      <div className="bottomright" style={{ width: "100%", height: "100%" }}>
-        <div style={{ overflow: "auto" }}>
-          <HexGrid>
-            <Layout size={{ x: 6, y: 6 }} spacing={1.1}>
-              {hexagons.map((hex, i) => (
-                <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s}>
-                  <Text>
-                    {i < datalist.length ? datalist[i].context : "Fuck"}
-                  </Text>
-                </Hexagon>
-              ))}
-            </Layout>
-          </HexGrid>
-          <TextField
+      <div className="bottomright" style={{ width: "100%", height: "90%", overflow:"scroll"}}>
+        <HexGridList>
+          {datalist.map((data, i) => (
+                (data.context != "")?<HexGridItem>
+                  {data.context}
+                </HexGridItem>:""
+          ))}
+        </HexGridList>
+      </div>
+      <div style={{ width: "100%"}}>
+        <TextField
             label={"Put Down Your Honey"}
             id="fullWidth"
             color="warning"
+            style={
+              {width:"100%",
+                // paddingTop:rem"
+              }
+            }
+            value={value} 
+            onChange={handleChange}
           />
-        </div>
       </div>
     </div>
   );
+
+
+  // <HexGrid>
+  //           <Layout size={{ x: 6, y: 6 }} spacing={1.1}>
+  //             <div style={{ overflowX: "scroll", width:"200%"}}>
+  //             {hexagons.map((hex, i) => (
+  //               <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s}>
+  //                 <Text>
+  //                   {i < datalist.length ? datalist[i].context : "Fuck"}
+  //                 </Text>
+  //               </Hexagon>
+  //             ))}
+  //             </div>
+  //           </Layout>
+  //         </HexGrid>
+  //         <TextField
+  //           label={"Put Down Your Honey"}
+  //           id="fullWidth"
+  //           color="warning"
+  //         />
 }
