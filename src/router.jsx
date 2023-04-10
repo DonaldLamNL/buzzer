169f6292db2933bdcb9f4e66702b7942e10e20cc
Circle-ma <a859576084@gmail.com>
2023-04-11 01:58:25 +0800

    lock at login page if not login

--- src/router.jsx
+++ src/router.jsx
@@ -1,62 +1,64 @@
 import Main from "./components/Main";
-import Home from "./components/Home/Home"
-import Buzz from "./components/BuzzPage/Buzz"
-import BuzzSearch from "./components/Searching/BuzzSearch"
+import Home from "./components/Home/Home";
+import Buzz from "./components/BuzzPage/Buzz";
+import BuzzSearch from "./components/Searching/BuzzSearch";
 import Login from "./components/User/Login";
 import Signup from "./components/User/Signup";
 import ForgotPassword from "./components/User/ForgotPassword";
 import ResetPassword from "./components/User/ResetPassword";
 import UserProfile from "./components/User/UserProfile";
 import EditProfile from "./components/User/EditProfile";
-import Hive from "./components/Hive"
+import Hive from "./components/Hive";
 
-const routerConfig = [
-  {
-    path: "/*",
-    element: <Main />,
-    children: [
-      {
-        path: "home/",
-        element: <Home />
-      },
-      {
-        path: "buzz",
-        element: <Buzz />,
-      },
-      {
-        path: "search",
-        element: <BuzzSearch />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/forgotpassword",
-    element: <ForgotPassword />,
-  },
-  {
-    path: "/resetpassword",
-    element: <ResetPassword />,
-  },
-  {
-    path: "/user",
-    element: <UserProfile />,
-  },
-  {
-    path: "/edit",
-    element: <EditProfile />,
-  },
-  {
-    path: "/hive",
-    element: <Hive />,
-  },
-];
+const routerConfig = (isLogin) => {
+  return [
+    {
+      path: "/*",
+      element: isLogin ? <Main /> : <Login />,
+      children: [
+        {
+          path: "home/",
+          element: isLogin ? <Home /> : null,
+        },
+        {
+          path: "buzz",
+          element: isLogin ? <Buzz /> : null,
+        },
+        {
+          path: "search",
+          element: isLogin ? <BuzzSearch /> : null,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: isLogin ? <Signup /> : <Login />,
+    },
+    {
+      path: "/forgotpassword",
+      element: isLogin ? <ForgotPassword /> : <Login />,
+    },
+    {
+      path: "/resetpassword",
+      element: isLogin ? <ResetPassword /> : <Login />,
+    },
+    {
+      path: "/user",
+      element: isLogin ? <UserProfile /> : <Login />,
+    },
+    {
+      path: "/edit",
+      element: isLogin ? <EditProfile /> : <Login />,
+    },
+    {
+      path: "/hive",
+      element: isLogin ? <Hive /> : <Login />,
+    },
+  ];
+};
 
 export default routerConfig;
