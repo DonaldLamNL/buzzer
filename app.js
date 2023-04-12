914a71352c38a8e2d553c47bb0d38b886401608b
Donald Lam <manholam8@gmail.com>
2023-04-12 20:25:04 +0800

    add dist

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Use Cors middleware
 const cors = require('cors')
